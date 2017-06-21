8d133a82d649356fc2b405a4073ca62bf3285e51
Alexander Smirnoff <a.dobeerman@gmail.com>
2017-06-21 22:09:10 +1000

    Added me some firebase

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 
 interface MovieQuote {
   movie: string;
   quote: string;
+  $key?: string;
 }
 
 @Component({
@@ -11,24 +13,43 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  readonly quotesPath = 'quotes';
+
   formMovieQuote: MovieQuote = {
-    quote: '',
-    movie: ''
+    'quote': '',
+    'movie': ''
   }
 
-  movieQuotes: Array<MovieQuote> = [
-    {movie: "Rocky", quote: "Yo Adrian"},
-    {movie: "Terminator", quote: "I'll be back"},
-    {movie: "Titanic", quote: "I'm the king of the world!"},
-    {movie: "The Princess Bride", quote: "Hello. My name is Indigo Montoya. You killed my father. Prepare to die."},
-  ]
+  // Local only solution
+  // movieQuotes: Array<MovieQuote> = [
+  //   { movie: "Rocky", quote: "Yo Adrian" },
+  //   { movie: "Terminator", quote: "I'll be back" },
+  //   { movie: "Titanic", quote: "I'm the king of the world!" },
+  //   { movie: "The Princess Bride", quote: "Hello. My name is Indigo Montoya. You killed my father. Prepare to die." },
+  // ];
+
+  movieQuotesStream: FirebaseListObservable<MovieQuote[]>;
+  constructor(db: AngularFireDatabase) {
+    try {
+      this.movieQuotesStream = db.list(this.quotesPath);
+    } catch (e) {
+      console.log("Error: ", e);
+    }
+  }
 
   onSubmit(): void {
-    console.log('TODO submit:', this.formMovieQuote);
-    this.movieQuotes.unshift(this.formMovieQuote);
-    this.formMovieQuote = {
-      quote: '',
-      movie: ''
-    };
+    // Local only solution
+    // this.movieQuotes.unshift(this.formMovieQuote);
+    try {
+      this.movieQuotesStream.push(this.formMovieQuote);
+
+      this.formMovieQuote = {
+        'quote': '',
+        'movie': ''
+      };
+    } catch (e) {
+      console.log("Form error: ", e);
+    }
   }
 }
